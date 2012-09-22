a1083a7daa50511beca9f8d30eeeb4b48ff6658b
chriz <shambler.qc@gmail.com>
2012-09-22 08:04:47 -0500

    adding a 256-color palette widget

---
+++ palette.py
@@ -0,0 +1,170 @@
+import collections
+import re
+
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+
+
+class ColorCell(QtGui.QPushButton):
+    hovered = QtCore.pyqtSignal(bool)
+
+    def __init__(self, color=None, parent=None):
+        super(ColorCell, self).__init__(parent=parent)
+
+        self.setColor(color)
+
+    def clearColor(self):
+        self.setColor(None)
+
+    def setColor(self, color):
+        if not color:
+            self.setStyleSheet("")
+        else:
+            self.setStyleSheet("* { background-color: rgb(%d,%d,%d) }" % (color.red(),
+                                                                          color.green(),
+                                                                          color.blue()))
+
+    def color(self):
+        pat = "rgb\\((\\d+),(\\d+),(\\d+)\\)"
+        m = re.compile(pat).search(self.styleSheet())
+        if not m:
+            return None
+        r, g, b = [int(m.group(gidx)) for gidx in (1, 2, 3)]
+        return QtGui.QColor(r, g, b)
+
+    def enterEvent(self, ev):
+        self.hovered.emit(True)
+        ev.accept()
+
+    def leaveEvent(self, ev):
+        self.hovered.emit(False)
+        ev.accept()
+
+
+class EditableRGBColorCell(ColorCell):
+    def mouseReleaseEvent(self, ev):
+        if ev.button() == QtCore.Qt.MiddleButton:
+            # Note we use self.parent() as the dialog's parent and not
+            # self. Using self causes the dialog's background to be the
+            # same as the button's background, making for a real shitty
+            # view.
+            if self.color() is not None:
+                dialog = QtGui.QColorDialog(self.color(), parent=self.parent())
+            else:
+                dialog = QtGui.QColorDialog(parent=self.parent())
+            if dialog.exec_():
+                self.setColor(dialog.selectedColor())
+            ev.accept()
+        else:
+            super(EditableRGBColorCell, self).mouseReleaseEvent(ev)
+
+
+class ReadOnlyColorCell(ColorCell):
+    def mousePressEvent(self, ev):
+        ev.ignore()
+
+
+class PaletteColorCell(ColorCell):
+    def sizeHint(self):
+        return QtCore.QSize(32, 32)
+
+
+class _PreviewCell(ReadOnlyColorCell):
+    def sizeHint(self):
+        return QtCore.QSize(128, 128)
+
+
+class IndexedColorPicker(QtGui.QWidget):
+    """
+    A interface to show a 256-color palette, where one cell is
+    selectable.
+    """
+
+    def __init__(self, colors={}, parent=None):
+        super(IndexedColorPicker, self).__init__(parent=parent)
+
+        # main color grid
+        self._buttongroup = QtGui.QButtonGroup(parent=self)
+        self._grid = QtGui.QGridLayout()
+        for r in xrange(16):
+            for c in xrange(16):
+                coloridx = r * 16 + c
+
+                cell = PaletteColorCell(color=colors.get(coloridx))
+                cell.setCheckable(True)
+                if (r, c) == (0, 0):
+                    cell.setChecked(True)
+                cell.hovered.connect(self._cellHovered)
+
+                self._buttongroup.addButton(cell, coloridx)
+
+                self._grid.addWidget(cell, r, c)
+
+        vbox = QtGui.QVBoxLayout()
+
+        # preview cell
+        self._preview = _PreviewCell()
+        vbox.addWidget(self._preview)
+
+        # text labels
+        self._labels = collections.OrderedDict()
+        self._labels["index"] = ("Index:", QtGui.QLabel("Index:"))
+        self._labels["red"]   = ("Red:",   QtGui.QLabel("Red:"))
+        self._labels["green"] = ("Green:", QtGui.QLabel("Green:"))
+        self._labels["blue"]  = ("Blue:",  QtGui.QLabel("Blue:"))
+        for lstart, l in self._labels.itervalues():
+            vbox.addWidget(l)
+
+        vbox.addStretch()
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addLayout(self._grid)
+        hbox.addLayout(vbox)
+
+        self.setLayout(hbox)
+
+    def selected(self):
+        return (self._buttongroup.checkedId(), self._buttongroup.checkedButton().color())
+
+    def _setupLabels(self, label_strings):
+        if not label_strings:
+            for lstart, l in self._labels.itervalues():
+                l.setText(lstart)
+        else:
+            for labelname, newval in label_strings.iteritems():
+                lstart, l = self._labels[labelname]
+                l.setText("%s %s" % (lstart, str(newval)))
+
+    def _cellHovered(self, is_on):
+        if is_on and self.sender().color():
+            l = {}
+            l["index"] = self._buttongroup.id(self.sender())
+            l["red"]   = self.sender().color().red()
+            l["green"] = self.sender().color().green()
+            l["blue"]  = self.sender().color().blue()
+            self._setupLabels(l)
+            self._preview.setColor(self.sender().color())
+        else:
+            self._setupLabels(None)
+            self._preview.clearColor()
+
+
+if __name__ == "__main__":
+    import sys
+    import random
+
+    randcolors = {i:QtGui.QColor(random.randint(0,255), random.randint(0,255), random.randint(0,255)) for i in xrange(18)}
+    randcolors[7] = None
+    randcolors[8] = None
+    randcolors[11] = None
+
+    app = QtGui.QApplication(sys.argv)
+
+    win = QtGui.QMainWindow()
+    win.setCentralWidget(IndexedColorPicker(colors=randcolors))
+    win.setWindowTitle("Palette Widget")
+    win.setWindowIcon(QtGui.QIcon(":/trolltech/qmessagebox/images/qtlogo-64.png"))
+    win.show()
+    win.resize(640, 480)
+
+    sys.exit(app.exec_())
